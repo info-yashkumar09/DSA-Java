9eba0441df53e2249eebce8138955dbf438c6852
Yash <info.yashkumar.09@gmail.com>
2026-04-16 12:28:16 +0530

    fcator

--- Loops/whileLoopProgram/FactorsOfNumber.java
+++ Loops/whileLoopProgram/FactorsOfNumber.java
@@ -2,12 +2,14 @@ package Loops.whileLoopProgram;
 
 public class FactorsOfNumber {
     public static void main(String[] args) {
-        int n =16; int i=1;
+        int n =16; int i=1; int count=0;
         while(i<=n){
             if(n%i==0){
                 IO.println(i);
+                count++;
             }
             i++;
         }
+        IO.println("Total factors of "+n+" are "+ count);
     }
 }
