f04f19026a0668345eb82619242544d673a785b7
Yash <info.yashkumar.09@gmail.com>
2026-04-22 14:26:31 +0530

    String Palindrome

--- DigitPrograms/CheckStringPalindrome.java
+++ DigitPrograms/CheckStringPalindrome.java
@@ -2,12 +2,16 @@ package DigitPrograms;
 
 public class CheckStringPalindrome {
     public static void main(String[] args) {
-        
+        IO.println(isStringPalindrome("madam"));
     }
 
     public static boolean isStringPalindrome(String s){
-        
-        return true;
+        int i=s.length()-1; String reverse="";
+        while(i>=0){
+            reverse+=s.charAt(i);
+            i--;
+        }
+        return s.equalsIgnoreCase(reverse);
     }
 
 }
