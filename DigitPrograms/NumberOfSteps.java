12f354f39fe71f8556d645e153b48e6f95c296dd
Yash <info.yashkumar.09@gmail.com>
2026-04-22 14:41:37 +0530

    number of steps to reduce to zero

---
+++ DigitPrograms/NumberOfSteps.java
@@ -0,0 +1,22 @@
+package DigitPrograms;
+
+public class NumberOfSteps {
+    public static void main(String[] args) {
+        IO.println(getNumberOfSteps(8));
+    }
+
+    public static int getNumberOfSteps(int n) {
+        int steps = 0;
+        while (n > 0) {
+            if ((n & 1) == 0) {
+                n /= 2;
+                steps++;
+            } else {
+                n--;
+                steps++;
+            }
+        }
+        return steps;
+    }
+
+}
